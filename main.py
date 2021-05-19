06103b4a4edb3d13034abd9f1a1559501ce9773e
hampytsi <vrukavik@icloud.com>
2021-05-19 21:43:33 +0300

    main code add

---
+++ main.py
@@ -0,0 +1,38 @@
+from flask import Flask, redirect,url_for, render_template, request
+
+app = Flask(__name__, template_folder='/Users/vladimirrukavisnikov/Documents/python_2021/templates')
+
+
+@app.route("/", methods=["POST", "GET"])  # this sets the route to this page
+def home():
+    if request.method == "POST":
+        user = request.form["nm"]
+        return redirect(url_for("user", usr=user))
+    else:
+        return render_template("index.html")
+
+
+@app.route("/login", methods=["POST", "GET"])
+def login():
+    if request.method == "POST":
+        user = request.form["nm"]
+        return redirect(url_for("user", usr=user))
+    else:
+        return render_template("login.html")
+
+
+@app.route("/<usr>", methods=["POST", "GET"])
+def user(usr):
+    if request.method == "POST":
+        if request.form["fruit"] == "банан":
+            return render_template("mumi_papa.html")
+        if request.form["fruit"] == "ананас":
+            return render_template("mumi_mama.html")
+        if request.form["fruit"] == "абрикос":
+            return render_template("little_my.html")
+    else:
+        return render_template("opros.html")
+
+
+if __name__ == "__main__":
+    app.run()
